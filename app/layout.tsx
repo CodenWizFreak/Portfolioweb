95a04efc16122a95ecbe424abb3646c2ea4234c0
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-09-11 22:41:53 +0530

    Updated Look

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next"
+import "../styles/globals.css" // Import globals.css here
+import RootLayout from "../src/app/layout"
+
+export const metadata: Metadata = {
+  title: "Ananyo Dasgupta - Portfolio",
+  description: "Personal portfolio website of Ananyo Dasgupta",
+    generator: 'v0.app'
+}
+
+export default RootLayout
